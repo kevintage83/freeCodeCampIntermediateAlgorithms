0302ebf2338f8027f027ab959991db046dfb2291
kevin <kevintage83@users.noreply.github.com>
2018-01-17 11:56:21 -0500

    Create diffTwoArrays.js

---
+++ diffTwoArrays.js
@@ -0,0 +1,16 @@
+function diffArray(arr1, arr2){
+  var newArray = [];
+  for(var i=0;i<arr1.length;i++){
+    if (arr2.indexOf(arr1[i]) === -1){
+    newArray.push(arr1[i]);
+    }
+  }
+  for(var j=0;j<arr2.length;j++){
+    if (arr1.indexOf(arr2[j]) === -1){
+    newArray.push(arr2[j]);
+    }
+  }
+  return newArray;
+}
+
+diffArray([1,2,3,4], [1,3,5,6]);
