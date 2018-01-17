da9315808c4ae95e94377534071b1ba6474e8d32
kevin <kevintage83@users.noreply.github.com>
2018-01-17 15:29:05 -0500

    Update diffTwoArrays.js

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -1,16 +1,39 @@
-function diffArray(arr1, arr2){
+//Create a new function, "diffArray," which takes two parameters, "arr1" and "arr2," as arguments. 
+function diffArray(array1, array2) {
+  //Create a new array "newArr"; This is where we will hold the results of the difference between the two parameters passed into diffArray.
   var newArray = [];
-  for(var i=0;i<arr1.length;i++){
-    if (arr2.indexOf(arr1[i]) === -1){
-    newArray.push(arr1[i]);
+  //Create a for-loop that loops through the first array, array1.
+  for(var i=0; i<array1.length; i++){
+    //Check to see if each item in array1 matches items in array2
+    //If there is a match, reject that match...
+    if(array2.indexOf(array1[i])===-1){
+      //Push the remaining items into newArray.
+      newArray.push(array1[i]);
     }
   }
-  for(var j=0;j<arr2.length;j++){
-    if (arr1.indexOf(arr2[j]) === -1){
-    newArray.push(arr2[j]);
+  //Create a for-loop that loops through the second array, array2.
+  for(var j=0; j<array2.length; j++){
+    //Check to see if each item in array2 matches items in array1.
+    if(array1.indexOf(array2[j])=== -1){
+      //Push the remaining items into newArray.
+      newArray.push(array2[j]);
     }
   }
+  //Return the results pushed into newArray.
   return newArray;
 }
+//Invoke the function "diffArray."
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
-diffArray([1,2,3,4], [1,3,5,6]);
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+*/
